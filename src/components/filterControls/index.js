479b55da108e7593628b6527f9fe951786f9e086
Steven McKeown <stevenmckeown19@gmail.com>
2020-11-01 18:26:12 +0000

    Refactor: Template list page; HTTP API calls centralized

--- src/components/filterControls/index.js
+++ src/components/filterControls/index.js
@@ -1,19 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./filterControls.css";
+import { getGenres } from "../../api/tmdb-api";
 
 const FilterControls = props => {
   const [genres, setGenres] = useState([{ id: '0', name: "All" }])
 
   useEffect(() => {
-    fetch(
-      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
-        process.env.REACT_APP_TMDB_KEY
-    )
-      .then(res => res.json())
-      .then(json => json.genres)
-      .then(apiGenres => {
-        setGenres([genres[0], ...apiGenres]);
-      });
+    getGenres().then(allGenres => {
+      setGenres([genres[0], ...allGenres]);
+    });
   }, []);
 
   const handleChange = (e, type, value) => {
@@ -28,27 +23,27 @@ const FilterControls = props => {
   };
 
   return (
-      <div className="row bg-warning">
-        <div className="col-md-12">
-          <h4>
-            <span>List Filtering:</span>
-            <input
-              type="text"
-              placeholder="Title Search"
-              onChange={handleTextChange}
-            />
+    <div className="row bg-warning">
+      <div className="col-md-12">
+        <h4>
+          <span>List Filtering:</span>
+          <input
+            type="text"
+            placeholder="Title Search"
+            onChange={handleTextChange}
+          />
           <select id="genre" onChange={handleGenreChange}>
-              {genres.map(genre => {
-                return (
-                  <option key={genre.id} value={genre.id}>
-                    {genre.name}
-                  </option>
-                );
-              })}
+            {genres.map(genre => {
+              return (
+                <option key={genre.id} value={genre.id}>
+                  {genre.name}
+                </option>
+              );
+            })}
           </select>
-         </h4>
-        </div>
+        </h4>
       </div>
+    </div>
   );
 }
 
