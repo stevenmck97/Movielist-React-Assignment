b25e47fae48957a396ce82ebe341c76f680b32d3
Steven McKeown <stevenmckeown19@gmail.com>
2020-11-06 16:40:24 +0000

    E2E tests - Home page - Filtering by genre

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -62,14 +62,27 @@ describe("Home Page ", () => {
                     cy.get(".card").each(($card, index) => {
                         cy.wrap($card)
                             .find(".card-title")
-                            .should("have.text", matchingMovies[index].title)
+                            .should.shouldn("have.text", matchingMovies[index].title)
                             .expect(matchingMovies.title.to.be.empty);
                     })
                 })
             })
             describe("By movie genre", () => {
-                // More later
-            })
-        })
+                it("should display movies with the specified genre only", () => {
+                  const selectedGenreId = 35;
+                  const selectedGenreText = "Comedy";
+                  const matchingMovies = filterByGenre(movies, selectedGenreId);
+                  cy.get("select").select(selectedGenreText); 
+                  cy.get(".card").should("have.length", matchingMovies.length);
+                  cy.get(".card").each(($card, index) => {
+                    cy.wrap($card)
+                      .find(".card-title")
+                      .should("have.text", matchingMovies[index].title);
+                  });      
+                });
+            });
+        });
     });
 });
+          
+        
