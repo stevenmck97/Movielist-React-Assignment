4abf874585e23ba8e980395d1a5e423dd2d8d366
Steven McKeown <stevenmckeown19@gmail.com>
2020-11-06 16:58:57 +0000

    E2E tests - Navigation - Favorites page

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -46,6 +46,20 @@ describe("Navigation", () => {
             cy.url().should("not.include", `/favorites`);
             cy.get("h2").contains("No. Movies");
         });
+        describe("From the Movie Details page ", () => {
+            beforeEach(() => {
+                cy.visit(`/movies/${movieId}`);
+            });
+            it("should change browser URL when show/hide reviews is clicked", () => {
+                cy.contains("Show Reviews").click();
+                cy.url().should("include", `/movies/${movieId}/reviews`);
+                cy.contains("Hide Reviews").click();
+                cy.url().should("not.include", `/movies/${movieId}/reviews`);
+            });
+            it("navigate to the full review page when a 'Full Review' link is clicked", () => {
+                // TODO
+            });
+        });
     });
-
 });
+
