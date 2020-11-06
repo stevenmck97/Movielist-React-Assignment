e3266d07ce5d9169b0968cfb9c1458b94ae3b4ba
Steven McKeown <stevenmckeown19@gmail.com>
2020-11-06 14:03:20 +0000

    Added Upcoming Movies Page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
             <Switch>
             <Route path="/reviews/:id" component={MovieReviewPage} />
             <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
+            <Route path="/movies/upcoming" component={UpcomingMoviesPage} />
             <Route path="/movies/:id" component={MoviePage} />
             <Route path="/" component={HomePage} />
             <Redirect from="*" to="/" />
