40b980aba641b61ce751d03cc457568104410722
Steven McKeown <stevenmckeown19@gmail.com>
2020-12-17 04:56:39 +0000

    changed wait function times to hopefully get the final test in the pipeline to pass again

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -29,7 +29,7 @@ describe("Navigation", () => {
     describe("From the home page", () => {
         beforeEach(() => {
             cy.visit("/");
-            cy.wait(3000)
+            cy.wait(6000)
         });
         it("should navigate to the movie details page and change browser URL", () => {
             cy.get(".card").eq(1).find("img").click();
@@ -88,6 +88,7 @@ describe("Navigation", () => {
         describe("From the Movie Details page ", () => {
             beforeEach(() => {
                 cy.visit(`/movies/${movieId}`);
+                cy.wait(6000)
             });
             it("should change browser URL when show/hide reviews is clicked", () => {
                 cy.contains("Show Reviews").click();
@@ -105,6 +106,7 @@ describe("Navigation", () => {
         describe("From the Favorites page", () => {
             beforeEach(() => {
                 cy.visit("/");
+                cy.wait(6000)
                 cy.get(".card").eq(0).find("button").click();
                 cy.get("button").contains("Movies").get("#dropdown-basic").click().get(".dropdown-item").contains("Favorite Movies").click();
             });
@@ -118,6 +120,7 @@ describe("Navigation", () => {
         describe("The Go Back button", () => {
             beforeEach(() => {
                 cy.visit("/");
+                cy.wait(6000)
             });
             it("should navigate from home page to movie details and back", () => {
                 cy.get(".card").eq(1).find("img").click();
