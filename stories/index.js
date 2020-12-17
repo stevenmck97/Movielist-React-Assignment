49473ddf9afeceb597cf7a73dac44b26215cacaf
Steven McKeown <stevenmckeown19@gmail.com>
2020-12-17 15:20:48 +0000

    added people story book support

--- stories/index.js
+++ stories/index.js
@@ -21,6 +21,16 @@ import TvHeader from "../src/components/headerTv";
 // import AddFavoriteButton from "../src/components/buttons/addToFavorites";
 import TvGenresContextProvider from "../src/contexts/genresContext";
 
+//people imports
+import PersonCard from "../src/components/personCard";
+import PeopleHeader from "../src/components/headerPersonList";
+import PersonList from "../src/components/personList";
+import PersonDetails from "../src/components/personDetails";
+import PersonHeader from "../src/components/headerPerson";
+// import AddFavoriteButton from "../src/components/buttons/addToFavorites";
+
+
+
 import { MemoryRouter } from "react-router";
 import { action } from "@storybook/addon-actions";
 
@@ -317,6 +327,34 @@ const tvSample = {
   "vote_count": 11504
 }
 
+const personSample = {
+  "birthday": "1963-12-18",
+  "known_for_department": "Acting",
+  "deathday": null,
+  "id": 287,
+  "name": "Brad Pitt",
+  "also_known_as": [
+    "برد پیت",
+    "Бред Питт",
+    "Бред Пітт",
+    "Buratto Pitto",
+    "Брэд Питт",
+    "畢·彼特",
+    "ブラッド・ピット",
+    "브래드 피트",
+    "براد بيت",
+    "แบรด พิตต์"
+  ],
+  "gender": 2,
+  "biography": "William Bradley \"Brad\" Pitt (born December 18, 1963) is an American actor and film producer. Pitt has received two Academy Award nominations and four Golden Globe Award nominations, winning one. He has been described as one of the world's most attractive men, a label for which he has received substantial media attention. Pitt began his acting career with television guest appearances, including a role on the CBS prime-time soap opera Dallas in 1987. He later gained recognition as the cowboy hitchhiker who seduces Geena Davis's character in the 1991 road movie Thelma & Louise. Pitt's first leading roles in big-budget productions came with A River Runs Through It (1992) and Interview with the Vampire (1994). He was cast opposite Anthony Hopkins in the 1994 drama Legends of the Fall, which earned him his first Golden Globe nomination. In 1995 he gave critically acclaimed performances in the crime thriller Seven and the science fiction film 12 Monkeys, the latter securing him a Golden Globe Award for Best Supporting Actor and an Academy Award nomination.\n\nFour years later, in 1999, Pitt starred in the cult hit Fight Club. He then starred in the major international hit as Rusty Ryan in Ocean's Eleven (2001) and its sequels, Ocean's Twelve (2004) and Ocean's Thirteen (2007). His greatest commercial successes have been Troy (2004) and Mr. & Mrs. Smith (2005).\n\nPitt received his second Academy Award nomination for his title role performance in the 2008 film The Curious Case of Benjamin Button. Following a high-profile relationship with actress Gwyneth Paltrow, Pitt was married to actress Jennifer Aniston for five years. Pitt lives with actress Angelina Jolie in a relationship that has generated wide publicity. He and Jolie have six children—Maddox, Pax, Zahara, Shiloh, Knox, and Vivienne.\n\nSince beginning his relationship with Jolie, he has become increasingly involved in social issues both in the United States and internationally. Pitt owns a production company named Plan B Entertainment, whose productions include the 2007 Academy Award winning Best Picture, The Departed.",
+  "popularity": 10.647,
+  "place_of_birth": "Shawnee, Oklahoma, USA",
+  "profile_path": "/kU3B75TyRiCgE270EyZnHjfivoq.jpg",
+  "adult": false,
+  "imdb_id": "nm0000093",
+  "homepage": null
+}
+
 //home page
 storiesOf("Home Page/MovieCard", module)
   .addDecorator(story => (
@@ -440,3 +478,65 @@ storiesOf("Tv Details Page/TvHeader", module)
     <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
   ))
   .add("default", () => <TvHeader tv={tvSample} />);
+
+
+  //popular actor pages
+storiesOf("Popular Actor Page/PersonCard", module)
+.addDecorator(story => (
+  <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+))
+.add("default", () => (
+  <PersonCard
+    person={personSample}
+    action={person => <button className="btn w-100 btn-primary">Test</button>}
+  />
+))
+.add("exception", () => {
+  const personSampleNoPoster = { ...personSample, profile_path: undefined };
+  return (
+    <PersonCard
+      person={personSampleNoPoster}
+      action={person => (
+        <button className="btn w-100 btn-primary">Test</button>
+      )}
+    />
+  );
+});
+
+storiesOf("Popular Actor Page/FilterControls", module)
+  .addDecorator(story => (
+    <TvGenresContextProvider>{story()}</TvGenresContextProvider>
+  ))
+  .add("default", () => (
+    <FilterControls onUserInput={action("button-click")} numTvShows={10} />
+  ));
+
+storiesOf("Popular Actor Page/PersonHeader", module).add("default", () => (
+  <PeopleHeader name="All Popular Actors" numPeople={10} />
+));
+
+storiesOf("Popular Actor Page/PersonList", module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+  ))
+  .add("default", () => {
+    const popular = [personSample, personSample, personSample, personSample, personSample];
+    return (
+      <PersonList
+      popular={popular}
+        action={person => (
+          <button className="btn w-100 btn-primary">Test</button>
+        )}
+      />
+    );
+  });
+
+storiesOf("Popular Actor Page/PersonDetails", module).add("default", () => (
+  <PersonDetails person={personSample} />
+));
+
+storiesOf("Popular Actor Page/PersonHeader", module)
+  .addDecorator(story => (
+    <MemoryRouter initialEntries={["/"]}>{story()}</MemoryRouter>
+  ))
+  .add("default", () => <PersonHeader person={personSample} />);
