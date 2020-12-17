c7b5f71ee577e0a58b94289524cd262d09180992
Steven McKeown <stevenmckeown19@gmail.com>
2020-12-17 04:12:38 +0000

    added testing in navigation for navigating to tv pages from the home page

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -38,21 +38,44 @@ describe("Navigation", () => {
         });
         it("should allow navigation from site header to movie dropdown pages", () => {
             cy.get("button").contains("Movies").get("#dropdown-basic").click().get(".dropdown-item").contains("Favorite Movies").click();
-            cy.url().should("include", `/favorites`);
+            cy.url().should("include", `movies/favorites`);
             cy.get("h2").contains("Favorite Movies");
 
             cy.get("button").contains("Movies").get("#dropdown-basic").click().get(".dropdown-item").contains("Upcoming Movies").click();
-            cy.url().should("include", `/upcoming`);
+            cy.url().should("include", `movies/upcoming`);
             cy.get("h2").contains("Upcoming Movies");
 
             cy.get("button").contains("Movies").get("#dropdown-basic").click().get(".dropdown-item").contains("Watchlist Movies").click();
-            cy.url().should("include", `/watchList`);
+            cy.url().should("include", `movies/watchList`);
             cy.get("h2").contains("Movies Watch List");
 
             cy.get("button").contains("Movies").get("#dropdown-basic").click().get(".dropdown-item").contains("Discover Movies").click();
             cy.url().should("include", `/`);
             cy.get("h2").contains("Discover Movies");
         });
+        it("should allow navigation from site header to tv shows dropdown pages", () => {
+            cy.get("button").contains("TV Shows").get("#dropdown-basic2").click().get(".dropdown-item").contains("Favorite TV Shows").click();
+            cy.url().should("include", `tv/favorites`);
+            cy.get("h2").contains("Favorite Tv Shows");
+
+            cy.get("button").contains("TV Shows").get("#dropdown-basic2").click().get(".dropdown-item").contains("Airing TV Shows").click();
+            cy.url().should("include", `tv/airing`);
+            cy.get("h2").contains("Airing Tv Shows");
+
+            cy.get("button").contains("TV Shows").get("#dropdown-basic2").click().get(".dropdown-item").contains("Top Rated TV Shows").click();
+            cy.url().should("include", `tv/topRated`);
+            cy.get("h2").contains("Top Rated Tv Shows");
+
+            cy.get("button").contains("TV Shows").get("#dropdown-basic2").click().get(".dropdown-item").contains("Watchlist TV Shows").click();
+            cy.url().should("include", `tv/watchList`);
+            cy.get("h2").contains("Tv Show Watch List");
+
+            cy.get("button").contains("TV Shows").get("#dropdown-basic2").click().get(".dropdown-item").contains("Discover TV Shows").click();
+            cy.url().should("include", `tv/discover`);
+            cy.get("h2").contains("Discover Tv Shows");
+        });
+     
+
         describe("From the Movie Details page ", () => {
             beforeEach(() => {
                 cy.visit(`/movies/${movieId}`);
